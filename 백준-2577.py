a45248c21c4a24ba407c4bed8cd4ad23c30fe385
eoaud0012 <eoaud0012@naver.com>
2021-05-18 20:50:01 +0900

    백준 알고리즘 - 숫자의 개수
    2577

---
+++ 백준-2577.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+result = a*b*c
+count_list = [0 for _ in range(10)]
+
+while(result > 0):
+    remain = int(result % 10)
+    count_list[remain] += 1
+    result = int(result / 10)
+
+for index in range(len(count_list)):
+    print(count_list[index])
