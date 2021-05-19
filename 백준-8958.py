e2ba493c03430f7b010861311cee21767dcddbae
eoaud0012 <eoaud0012@naver.com>
2021-05-19 21:30:01 +0900

    백준 알고리즘 - OX퀴즈
    8958

---
+++ 백준-8958.py
@@ -0,0 +1,51 @@
+problem_count = int(input())
+problem_list = [list(input()) for _ in range(problem_count)]
+
+def score_inspector():
+    i = 0
+    j = 0
+    score_sum = 0
+    common_difference = 0
+    for i in range(len(problem_list)):
+
+        for j in range(len(problem_list[i])):
+            if(problem_list[i][j] == 'O'):
+                common_difference += 1
+                score_sum += common_difference
+            else:
+                common_difference = 0
+
+        print(score_sum)
+        score_sum = 0
+        common_difference = 0
+    return
+
+score_inspector()
+
+    
+
+
+
+# 파이썬에서 H x W 2차원 리스트를 0으로 초기화 하려면 다음과 같이 하면 됩니다.
+
+# arr = [[0 for i in range(w)] for j in range(h)]
+
+# 혹은
+
+# arr = [[0]*w for j in range(h)]​
+
+# 당연히 1차원 리스트는 다음과 같이.
+
+# list1 = [0 for i in range(w)]
+
+# 2차원 리스트 선언만 하려면 아래와 같이.
+
+# data = [[] * n for i in range(n)]    # data: [[], [], [], []]
+
+# * 추가 
+
+#  - 1차원배열 초기화 
+
+#     arr = [0] * n 
+# [출처] [Python] 2차원 리스트 초기화|작성자 뚝이파파
+#
