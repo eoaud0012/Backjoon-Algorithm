10797084dac2eb4009cc92f46c55f27997666b3f
eoaud0012 <eoaud0012@naver.com>
2021-05-19 21:30:27 +0900

    Update 백준-2577.py
    배열 초기화 방법 주석 추가

--- 백준-2577.py
+++ 백준-2577.py
@@ -3,7 +3,8 @@ b = int(input())
 c = int(input())
 
 result = a*b*c
-count_list = [0 for _ in range(10)]
+count_list = [0 for _ in range(10)] # 1차원배열 초기화  -- > arr = [0] * n 
+
 
 while(result > 0):
     remain = int(result % 10)
